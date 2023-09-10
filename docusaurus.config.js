9e59f48ee79e614318884f891cb05a20d8cd8529
Pavel Shut <sp.shut@gmail.com>
2023-09-10 16:51:28 +0200

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,27 +53,23 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
+      // navbar: {
+      //   title: 'Cancel Cancer',
+      //   items: [
+      //     {
+      //       type: 'docSidebar',
+      //       sidebarId: 'tutorialSidebar',
+      //       position: 'left',
+      //       label: 'Tutorial',
+      //     },
+      //     {to: '/blog', label: 'Blog', position: 'left'},
+      //     {
+      //       href: 'https://github.com/facebook/docusaurus',
+      //       label: 'GitHub',
+      //       position: 'right',
+      //     },
+      //   ],
+      // },
       footer: {
         style: 'dark',
         links: [
